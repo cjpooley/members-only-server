8581216fd8a94b106a23c32ea2763dc69d2c6cb3
Chris Pooley <cpooley@gmail.com>
2021-05-06 21:28:34 -0700

    Complete

--- src/db/index.js
+++ src/db/index.js
@@ -1,4 +1,7 @@
 export { db } from './db';
+export { acceptRequest } from './acceptRequest';
+export { rejectRequest } from './rejectRequest';
+export { getRequestsForGroup } from './getRequestsForGroup';
 export { getAllGroups } from './getAllGroups';
 export { getUserGroups } from './getUserGroups';
 export { createJoinRequest } from './createJoinRequest';
@@ -6,3 +9,5 @@ export { createGroup } from './createGroup';
 export { getGroup } from './getGroup';
 export { getMemberPopulatedGroup } from './getMemberPopulatedGroup';
 export { getOwnerPopulatedGroup } from './getOwnerPopulatedGroup';
+export { addMessageToGroup } from './addMessageToGroup';
+export { getMessagesForGroup } from './getMessagesForGroup';
